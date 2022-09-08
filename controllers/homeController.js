080e0ec3bdd79147d911c30cc95e99d12c32bb44
mdatif796 <mdatif796@gmail.com>
2022-09-08 17:32:54 +0530

    populating post with their user

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,15 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    res.render('home', {
-        title: "home"
+    // populate here is used to pass all the user information 
+    Post.find({}).populate('user').exec(function(err, posts){
+        if(err){
+            console.log('Error in finding all posts of home page.');
+            return;
+        }
+        return res.render('home', {
+            title: "iConnect | home",
+            posts: posts
+        });
     });
 }
